97c3381bc0f4f4bec95ec217350f6a4742284703
1qlee <wonq33@gmail.com>
2019-10-29 18:25:40 -0400

    First Commit

---
+++ gatsby-config.js
@@ -0,0 +1,55 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+module.exports = {
+  siteMetadata: {
+    title: `Notesmith`,
+    description: `Custom printed notebooks made for you because they're made by you. Design and create your Notesmith notebook today!`,
+    author: `@notesmith`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        downloadLocal: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Karla\:400,700`,
+          `Lora\:400,700`
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Notesmith`,
+        short_name: `Notesmith`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>{`${process.env.GATSBY_WELCOME_MESSAGE}`}</h1>
+    <div>
+      {data.allContentfulProduct.edges.map(({ node: product}) => (
+        <p>{product.productName}</p>
+      ))}
+    </div>
+    <Link to="/account">Log In</Link>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export const query = graphql`
+  query IndexQuery {
+    allContentfulProduct {
+      edges {
+        node {
+          id
+          productName
+        }
+      }
+    }
+  }
+`
+
+
+export default IndexPage
