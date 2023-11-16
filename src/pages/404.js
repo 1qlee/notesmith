03f9e6bc17b789eb70751110eea720a6dd12a765
Wonkyu Lee <wonq33@gmail.com>
2023-11-16 01:22:44 -0500

    Attempting fix 2

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { spacing } from "../styles/variables"
+import { isBrowser } from "../utils/helper-functions"
 
 import Seo from "../components/layout/Seo"
 import Layout from "../components/layout/Layout"
@@ -8,29 +9,31 @@ import { Section, SectionMain, SectionContent } from "../components/layout/Secti
 import { Container, Row, Col } from "react-grid-system"
 
 const NotFoundPage = () => {
-  return (
-    <Layout>
-      <SectionMain
-        className="has-max-height"
-      >
-        <Section>
-          <SectionContent
-            padding={`${spacing.section} 0`}
-          >
-            <Container xl lg md sm xs>
-              <Row>
-                <Col>
-                  <Content>
-                    <h1>Page not found</h1>
-                  </Content>
-                </Col>
-              </Row>
-            </Container>
-          </SectionContent>
-        </Section>
-      </SectionMain>
-    </Layout>
-  )
+  if (isBrowser()) {
+    return (
+      <Layout>
+        <SectionMain
+          className="has-max-height"
+        >
+          <Section>
+            <SectionContent
+              padding={`${spacing.section} 0`}
+            >
+              <Container xl lg md sm xs>
+                <Row>
+                  <Col>
+                    <Content>
+                      <h1>Page not found</h1>
+                    </Content>
+                  </Col>
+                </Row>
+              </Container>
+            </SectionContent>
+          </Section>
+        </SectionMain>
+      </Layout>
+    )
+  }
 }
 
 export default NotFoundPage
