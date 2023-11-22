9956cd46443c51b6283667987b13a02b3c21c7cb
Wonkyu Lee <wonq33@gmail.com>
2023-11-22 00:30:53 -0500

    Attempt at loading stripe off the main thread with gatsby script library

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,24 +1,28 @@
-import React, { lazy, Suspense } from "react"
+import React from "react"
+import { Script } from "gatsby"
 import { FirebaseProvider } from "./src/utils/auth"
 import { CartProvider } from "use-shopping-cart"
-import Loader from "./src/components/misc/Loader"
+import getStripe from "./src/utils/stripejs"
 
 export const wrapRootElement = ({ element, props }) => {
-  const stripe = lazy(() => import("./src/utils/stripejs"))
-  console.log("🚀 ~ file: gatsby-browser.js:8 ~ wrapRootElement ~ stripe:", stripe)
+  const stripe = getStripe()
 
   return (
-    <Suspense fallback={Loader}>
-      <CartProvider
-        allowedCountries={['US']}
-        billingAddressCollection={true}
-        currency="USD"
-        mode="client-session"
-        stripe={stripe}
-        successUrl="stripe.com"
-      >
-        <FirebaseProvider {...props}>{element}</FirebaseProvider>
-      </CartProvider>
-    </Suspense>
+    <CartProvider
+      allowedCountries={['US']}
+      billingAddressCollection={true}
+      currency="USD"
+      mode="client-session"
+      successUrl="stripe.com"
+      stripe={stripe}
+    >
+      <FirebaseProvider {...props}>
+        {element}
+        <Script 
+          src="https://js.stripe.com/v3" 
+          strategy="off-main-thread"
+        />
+      </FirebaseProvider>
+    </CartProvider>
   )
 }
