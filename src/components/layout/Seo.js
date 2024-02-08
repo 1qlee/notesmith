d008e6817688010fda29dbe1839e3d9dbfca6b81
Wonkyu Lee <wonq33@gmail.com>
2024-02-08 13:49:22 -0500

    Change helmet props to elements

--- src/components/layout/Seo.js
+++ src/components/layout/Seo.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Helmet from "react-helmet"
+import { Helmet } from "react-helmet"
 import { useSiteMetadata } from "../../hooks/useSiteMetadata"
 
 function Seo({ details, hideScroll }) {
@@ -21,54 +21,25 @@ function Seo({ details, hideScroll }) {
       htmlAttributes={{
         lang: "en",
       }}
-      title={`${title} | Notesmith`}
-      meta={[
-        {
-          name: `description`,
-          content: seo.description,
-        },
-        {
-          property: `og:title`,
-          content: seo.title,
-        },
-        {
-          property: `og:url`,
-          content: `${seo.url}`,
-        },
-        {
-          property: `og:description`,
-          content: seo.description,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          property: `og:image`,
-          content: `${seo.image}`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:url`,
-          content: `${seo.url}`,
-        },
-        {
-          name: `twitter:image`,
-          content: `${seo.image}`,
-        },
-        {
-          name: `twitter:title`,
-          content: seo.title,
-        },
-        {
-          name: `twitter:description`,
-          content: seo.description,
-        },
-      ]}
-    />
+    >
+      <title>{title}</title>
+      <meta name="description" content={seo.description} />
+      <meta name="image" content={seo.image} />
+      <link rel="canonical" href={seo.url} />
+
+      {/* OpenGraph tags */}
+      <meta property="og:url" content={seo.url} />
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:description" content={seo.description} />
+      <meta property="og:image" content={seo.image} />
+
+      {/* Twitter Card tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content="Notesmith" />
+      <meta name="twitter:title" content={seo.title} />
+      <meta name="twitter:description" content={seo.description} />
+      <meta name="twitter:image" content={seo.image} />
+    </Helmet>
   )
 }
 
