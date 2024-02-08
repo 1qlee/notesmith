c8966408b29405855eb8d52bba02cf33264a6ad5
Wonkyu Lee <wonq33@gmail.com>
2024-02-08 14:32:53 -0500

    Removed toastify css from layout

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -2,7 +2,6 @@ import React from "react"
 import styled from "styled-components"
 import { colors } from "../../styles/variables"
 import useIsClient from "../../hooks/useIsClient"
-import 'react-toastify/dist/ReactToastify.css'
 import "./master.css"
 
 import Nav from "./Nav"
