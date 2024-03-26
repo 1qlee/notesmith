264bf3e99b6c1f062ad122e8e81bcb8b84ab7240
Wonkyu <wonq33@gmail.com>
2024-03-26 11:42:29 -0400

    Changed gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,35 @@ module.exports = {
     image: "/meta-image.jpg"
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-6P3H5Q84XJ", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // gtagConfig: {
+        //   optimize_id: "GTM-T98ZLS2V",
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // // Setting this parameter is also optional
+          // respectDNT: true,
+          // // Avoids sending pageview hits from custom paths
+          // exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // // Defaults to https://www.googletagmanager.com
+          // origin: "https://www.notesmithbooks.com",
+          // // Delays processing pageview events on route update (in milliseconds)
+          // delayOnRouteUpdate: 0,
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: 'gatsby-plugin-robots-txt',
@@ -110,35 +139,6 @@ module.exports = {
           ],
         }
       }
-    },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "G-9S05PJ9HT1", // Google Analytics / GA
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        // gtagConfig: {
-        //   optimize_id: "GTM-T98ZLS2V",
-        //   anonymize_ip: true,
-        //   cookie_expires: 0,
-        // },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: true,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-          // Defaults to https://www.googletagmanager.com
-          origin: "https://www.notesmithbooks.com",
-          // Delays processing pageview events on route update (in milliseconds)
-          delayOnRouteUpdate: 0,
-        },
-      },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
